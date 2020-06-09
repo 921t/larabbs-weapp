df87ae0eee7157bb40117e5791c2ab7d64b4a00d
921t <1844325217@qq.com>
2020-06-09 16:25:31 +0800

    topic-list component

--- src/api/topic.js
+++ src/api/topic.js
@@ -1,4 +1,4 @@
-import {request} from '@/utils/request'
+import {request, authRequest} from '@/utils/request'
 
 export function getTopics(data) {
   return request('topics', {
@@ -13,5 +13,11 @@ export function getCategories(data) {
 export function getTopic(id, data) {
   return request('topics/' + id, {
     data: data
+  })
+}
+
+export function getUserTopics(userId, data) {
+  return authRequest('users/' + userId + '/topics', {
+    data
   })
 }
